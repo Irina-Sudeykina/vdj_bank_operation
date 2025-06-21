c1fd3d7d64e7209143fa3b51d393bb3fdb40b1dd
Ирина Судейкина <irinaasster@gmail.com>
2025-06-21 17:57:56 +0500

    add  tests/my_test_use_file_names.py

--- src/use_file_names.py
+++ src/use_file_names.py
@@ -8,19 +8,19 @@ def get_names_list(use_file_name: str) -> list[str]:
     :param use_file_name: имя файла
     :return: список имен из указанного файла
     """
-    if os.path.exists(use_file_name) == False:
-        return "Такого файла не существует."
+    if not os.path.exists(use_file_name):
+        return ["Такого файла не существует."]
 
     result_list_names = []
     del_chr = "0123456789,.|/!()[]{};:'&?@#№%^&*"
 
-    with open(use_file_name, 'r', encoding='utf-8') as file:
+    with open(use_file_name, "r", encoding="utf-8") as file:
         list_mames_of_file = file.readlines()
 
     for i in list_mames_of_file:
         tmp_name = i.strip()
         for j in del_chr:
-            tmp_name = tmp_name.replace(j,"")
+            tmp_name = tmp_name.replace(j, "")
         if tmp_name != "":
             result_list_names.append(tmp_name)
 
---
+++ tests/my_test_use_file_names.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+from src import use_file_names
+
+# Задаём корневой путь проекта.
+root_path = Path(__file__).resolve().parents[1]
+print(root_path)
+file_of_names = os.path.join(root_path, "data\\names.txt")
+print(file_of_names)
+print(use_file_names.get_names_list(file_of_names))
