b1dded7d7e930118af0243b5e13b72346aa4dbeb
Ирина Судейкина <irinaasster@gmail.com>
2025-06-22 01:14:14 +0500

    add get_date in src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src import masks
 
 
@@ -13,3 +15,13 @@ def mask_account_card(acc_card: str) -> str:
         return f"{acc_card[:(len(acc_card) - 20)]}{masks.get_mask_account(int(acc_card[-20:]))}"
     else:
         return f"{acc_card[:(len(acc_card) - 16)]}{masks.get_mask_card_number(int(acc_card[-16:]))}"
+
+
+def get_date(use_iso_date: str) -> str:
+    """
+    Функция принимает строку в формате 2024-03-11T02:26:18.671407
+    и возвращает строку с датой в формате ДД.ММ.ГГГГ (11.03.2024)
+    :param use_iso_date: строка в формате 2024-03-11T02:26:18.671407
+    :return: строка с датой в формате ДД.ММ.ГГГГ (11.03.2024)
+    """
+    return datetime.fromisoformat(use_iso_date).strftime("%d.%m.%Y")
