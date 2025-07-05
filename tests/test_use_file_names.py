26a84406aeda82f20ad6c351b15878ed1781923f
Ирина Судейкина <irinaasster@gmail.com>
2025-07-05 23:47:49 +0500

    apdate conftest and test use_file_names

--- tests/test_use_file_names.py
+++ tests/test_use_file_names.py
@@ -8,13 +8,14 @@ root_path = Path(__file__).resolve().parents[1]
 file_of_names = os.path.join(root_path, "data\\names.txt")
 
 
-def test_get_names_list_02(cleaned_name_list: list[str]) -> None:
+def test_get_names_list(cleaned_name_list: list[str]) -> None:
     """
     Проверка функции get_names_list_02,
     которая должна возвращать очищеный список имен
     :param cleaned_name_list: фикстура с очищенным списком имен
     :return: очищеный список имен
     """
+    assert use_file_names.get_names_list(file_of_names) == cleaned_name_list
     assert use_file_names.get_names_list_02(file_of_names) == cleaned_name_list
 
 
@@ -31,3 +32,32 @@ def test_get_list_of_lang_names(
     """
     assert use_file_names.get_list_of_lang_names(cleaned_name_list, "eng") == eng_name_list
     assert use_file_names.get_list_of_lang_names(cleaned_name_list) == rus_name_list
+
+
+def test_set_list_to_file(
+    eng_name_list: list[str], rus_name_list: list[str], eng_data_file: str, rus_data_file: str
+) -> None:
+    """
+    Проверка функции set_list_to_file,
+    которая должна записывать список имен в файл
+    :param eng_name_list: фикстура со списком имен на английском
+    :param rus_name_list: фикстура со списком имен на русском
+    :param eng_data_file: фикстура содержимого файла с именами на английском
+    :param rus_data_file: фикстура содержимого файла с именами на русском
+    :return: файл со списком имен
+    """
+    file_of_eng_names = os.path.join(root_path, "data\\eng_names.txt")
+    file_of_rus_names = os.path.join(root_path, "data\\rus_names.txt")
+    use_file_names.set_list_to_file(eng_name_list, file_of_eng_names)
+    use_file_names.set_list_to_file(rus_name_list, file_of_rus_names)
+
+    assert os.path.exists(file_of_eng_names)
+    assert os.path.exists(file_of_rus_names)
+
+    with open(file_of_eng_names, "r", encoding="utf-8") as file:
+        data_value = file.read()
+        assert data_value == eng_data_file
+
+    with open(file_of_rus_names, "r", encoding="utf-8") as file:
+        data_value = file.read()
+        assert data_value == rus_data_file
