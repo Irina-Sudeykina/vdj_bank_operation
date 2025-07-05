5af98be6c01779abd2b8c0f521995a4f5b9df6c3
Ирина Судейкина <irinaasster@gmail.com>
2025-07-05 19:57:22 +0500

    add test function get_list_of_lang_names

--- tests/test_use_file_names.py
+++ tests/test_use_file_names.py
@@ -16,3 +16,18 @@ def test_get_names_list_02(cleaned_name_list: list[str]) -> None:
     :return: очищеный список имен
     """
     assert use_file_names.get_names_list_02(file_of_names) == cleaned_name_list
+
+
+def test_get_list_of_lang_names(
+    cleaned_name_list: list[str], eng_name_list: list[str], rus_name_list: list[str]
+) -> None:
+    """
+    Проверка функции get_list_of_lang_names,
+    которая должна возвращать список с именами только на английском или только на русском
+    :param cleaned_name_list: фикстура с очищенным списком имен
+    :param eng_name_list: фикстура со списком имен на английском
+    :param rus_name_list: фикстура со списком имен на русском
+    :return: список имен на русском или на английском
+    """
+    assert use_file_names.get_list_of_lang_names(cleaned_name_list, "eng") == eng_name_list
+    assert use_file_names.get_list_of_lang_names(cleaned_name_list) == rus_name_list
